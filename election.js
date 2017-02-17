c5933065f10e54abafca9fec2b7056e970bc4dc8
yonas samuel <yonastlolo@yahoo.com>
2017-02-17 15:13:19 -0500

    bikini-part-1

--- election.js
+++ election.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
   // Imagination!
+  $.ajax({
+   url: 'https://bb-election-api.herokuapp.com/',
+   method:'GET',
+   dataType: 'json'
 
+
+ }).done(function(data){
+    $(data.candidates).each(function(candidate) {
+      $('<li>').html('Candidate: ' + this.name + '  ' + ' Votes: ' + this.votes).appendTo('#candidates');
+    });
+  });
 });
